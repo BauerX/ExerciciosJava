6935518960df0c31e2a9d41959f0d91127080cd1
EDUARDO BAUER <edu.bauer1@gmail.com>
2015-12-12 21:41:57 -0200

    Exercícios Java-Basico

---
+++ Exercicio2.java
@@ -0,0 +1,30 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Qual a sua idade?: ");
+		int idade = scan.nextInt();
+		
+		System.out.println("Qual a idade da sua esposa: ");
+		int idade2 = scan.nextInt();
+		
+		int resultado = idade + idade2;
+		System.out.println("A sua idade somada a da sua esposa é: " + resultado);
+		
+	
+		
+		
+	
+	
+		
+	
+
+	}
+
+}
---
+++ Exercicio4.java
@@ -0,0 +1,29 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+
+	public static void main(String[] args) {
+	
+	Scanner scan = new Scanner(System.in);
+	
+	System.out.println("Nota do Aluno1");
+	float aluno1 = scan.nextFloat();
+	System.out.println("Nota do Aluno2");
+	float aluno2 = scan.nextFloat();
+	System.out.println("Nota do Aluno3");
+	float aluno3 = scan.nextFloat();
+	System.out.println("Nota do Aluno4");
+	float aluno4 = scan.nextFloat();
+	
+	float resultado = aluno1 + aluno2 + aluno3 + aluno4 / 4;
+	
+	System.out.println("A média de notas é: " + resultado);
+	
+	
+	
+
+	}
+
+}
