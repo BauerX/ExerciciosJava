64d1c544449f187be9da541d421cea9be39e108f
EDUARDO BAUER <edu.bauer1@gmail.com>
2015-12-12 22:45:28 -0200

    Exericio5
    transformando metros em centímetros

---
+++ Exericio5.java
@@ -0,0 +1,25 @@
+//transformando metros em centimetros
+
+
+package exercicio1;
+
+import java.util.Scanner;
+
+public class Exericio5 {
+
+	public static void main(String[] args) {
+	
+	Scanner scan = new Scanner (System.in);
+	
+	System.out.println("Coloque o valor em metros:");
+	float metros = scan.nextFloat();
+	
+	System.out.println("O resultado em centimetros é: " + metros *100+ "cm");
+	float centimetros = scan.nextFloat();
+	
+
+	
+
+	}
+
+}
